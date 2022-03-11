c7980947aa3a785405814bc564e425c93e60a583
yichunzhao <zhaoyichun@gmail.com>
2022-03-12 00:44:25 +0100

    Initial commit

---
+++ SplitHugeList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SplitHugeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() {2, 4, 90, 30, 40, 29, 76, 33, 12, 34};
+            Console.WriteLine(string.Join(",",numbers));
+
+            // for (int i = 0; i < numbers.Count; i++)
+            // {
+            //     Console.WriteLine(numbers[i]);
+            // }
+        }
+    }
+}
